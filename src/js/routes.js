1c5462190f4e8a5750fd07edb55d79a4c33edda8
jerryzhch <jerry_agboola@hotmail.com>
2021-09-25 02:35:07 +0200

    Basic structure

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,10 +1,20 @@
 
-import HomePage from '../pages/home.f7';
+import LoadingPage from '../pages/loading.f7';
+import LoginPage from '../pages/login.f7';
+import MainPage from '../pages/home.f7';
 
 var routes = [
   {
     path: '/',
-    component: HomePage,
+    component: LoadingPage,
+  },
+  {
+    path: '/login',
+    component: LoginPage,
+  },
+  {
+    path: '/home',
+    component: MainPage,
   },
 ];
 
