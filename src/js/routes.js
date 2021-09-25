9072d3be0617a59947527ae48ae7c03d4d045753
Florian Thiévent <florianjeremias@gmail.com>
2021-09-25 13:41:40 +0200

    started with loading Screen

--- src/js/routes.js
+++ src/js/routes.js
@@ -2,19 +2,18 @@ import LoadingPage from "../pages/loading.f7";
 import LoginPage from "../pages/login.f7";
 import MainPage from "../pages/home.f7";
 
-var routes = [
-  {
-    path: "/",
-    component: LoginPage,
-  },
-  {
-    path: "/loading",
-    component: LoadingPage,
-  },
-  {
-    path: "/home",
-    component: MainPage,
-  },
+var routes = [{
+        path: "/login",
+        component: LoginPage,
+    },
+    {
+        path: "/",
+        component: LoadingPage,
+    },
+    {
+        path: "/home",
+        component: MainPage,
+    },
 ];
 
 export default routes;
