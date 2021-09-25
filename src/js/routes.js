80ee5fe467a0c9ed9ee188f630b17329f1a61a53
jerryzhch <jerry_agboola@hotmail.com>
2021-09-25 10:52:11 +0200

    Added Login Screen Ui #1

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,19 +1,18 @@
-
-import LoadingPage from '../pages/loading.f7';
-import LoginPage from '../pages/login.f7';
-import MainPage from '../pages/home.f7';
+import LoadingPage from "../pages/loading.f7";
+import LoginPage from "../pages/login.f7";
+import MainPage from "../pages/home.f7";
 
 var routes = [
   {
-    path: '/',
-    component: LoadingPage,
-  },
-  {
-    path: '/login',
+    path: "/",
     component: LoginPage,
   },
   {
-    path: '/home',
+    path: "/loading",
+    component: LoadingPage,
+  },
+  {
+    path: "/home",
     component: MainPage,
   },
 ];
